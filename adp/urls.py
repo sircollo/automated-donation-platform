9de8b7597c7bc89f7ca32cd4c9630f59f1c4254f
Collins Sirwani <sircollo71@gmail.com>
2022-07-04 14:11:18 +0300

    Feat: add urls for signin and signup

--- adp/urls.py
+++ adp/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
+from .views import *
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signin/', GetTokenPairView.as_view(), name='token_obtain_pair'),
+    path('signup/', RegisterDonorView.as_view(), name='signup'),
+    path('signup-charity/', RegisterCharityView.as_view(), name='signup'),
 ]
+
+# API URLS
+# signin - http://127.0.0.1:8000/signin/
+# signup-donor - http://127.0.0.1:8000/signup/
+# signup-charity - http://127.0.0.1:8000/signup-charity/
