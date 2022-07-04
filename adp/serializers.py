367894affdf37917dc12cc7f81ea3f30e4481290
Collins Sirwani <sircollo71@gmail.com>
2022-07-04 14:13:25 +0300

    Feat: add serializer file, serialize user login, donor and charity registration

---
+++ adp/serializers.py
@@ -0,0 +1,85 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.validators import UniqueValidator 
+from django.contrib.auth.password_validation import validate_password
+from .models import *
+
+
+# get user token
+class GetTokenPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super(GetTokenPairSerializer, cls).get_token(user)
+
+        token['username'] = user.username
+        print(user.is_donor)
+        return token      
+      
+# Register Donor 
+class RegisterDonorSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True,validators=[UniqueValidator(queryset=User.objects.all())])
+    password1 = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username','email','password1', 'password2')
+
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True}
+        }
+
+    def validate(self, attrs):
+        if attrs['password1'] != attrs['password2']:
+            raise serializers.ValidationError({"password1": "Password fields didn't match."})
+
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            is_donor=True
+        )
+
+        
+        user.set_password(validated_data['password1'])
+        user.is_donor = True
+        user.save()
+
+        return user 
+
+# Register Charity  
+class RegisterCharitySerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True,validators=[UniqueValidator(queryset=User.objects.all())])
+    password1 = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+    def validate(self, attrs):
+        if attrs['password1'] != attrs['password2']:
+            raise serializers.ValidationError({"password1": "Password fields didn't match."})
+
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            is_charity=True
+        )
+
+        
+        user.set_password(validated_data['password1'])
+        user.is_charity = True
+        user.save()
+
+        return user
