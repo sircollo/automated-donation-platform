e94c57fcec895d8ea0dd1f651944b6e92950f233
Collins Sirwani <sircollo71@gmail.com>
2022-07-04 14:09:48 +0300

    Feat: add create api view for user login, donor and charity registration

--- adp/views.py
+++ adp/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render, redirect
 from django.http.response import HttpResponse, Http404
-
+from .serializers import *
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny
+from rest_framework import generics
 # Create your views here.
 
 def index(request):
     return HttpResponse('Welcome to Fundflow')
 
+# Get token/login
+class GetTokenPairView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+    serializer_class = GetTokenPairSerializer
+    
+# Register donor view
+class RegisterDonorView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterDonorSerializer
+    
+# Register charity view 
+class RegisterCharityView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterCharitySerializer
+  
