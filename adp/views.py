123f6637465a332378ee8a6bcf768a3cd2ad6d83
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-07-05 10:08:48 +0300

    create the posts API

--- adp/views.py
+++ adp/views.py
@@ -116,3 +116,45 @@ class DonationsDetails(APIView):
         donation = self.get_object(pk)
         donation.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class PostsList(APIView):
+    def get(self, request):
+        posts = Posts.objects.all()
+        serializer = PostsSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PostsDetails(APIView):
+    def get_object(self, pk):
+        try:
+            return Posts.objects.get(pk=pk)
+        except Posts.DoesNotExist:
+            raise Http404
+
+    #To get a particular post
+    def get(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostsSerializer(post)
+        return Response(serializer.data)
+
+    #To update a particular post
+    def put(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostsSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    #To delete a particular post
+    def delete(self, request, pk, format=None):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
