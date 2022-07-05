04090e26f7fe67ecd20fbd59672ae5b35d5749eb
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-07-05 10:20:52 +0300

    create the api routes

--- adp/urls.py
+++ adp/urls.py
@@ -3,6 +3,20 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    #charities API endpoints
     path('api/charities/', views.CharityList.as_view()),
     path('api/charities/<int:pk>/', views.CharityDetails.as_view()),
+
+    #donations API endpoints
+    path('api/donations/', views.DonationList.as_view()),
+    path('api/donations/<int:pk>/', views.DonationDetails.as_view()),
+
+    #feedback API endpoints
+    path('api/feedback/', views.FeedbackList.as_view()),
+    path('api/feedback/<int:pk>/', views.FeedbackDetails.as_view()),
+
+    #posts API endpoints
+    path('api/posts/', views.PostList.as_view()),
+    path('api/posts/<int:pk>/', views.PostDetails.as_view()),
 ]
