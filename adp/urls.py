de3b0e2ab6122352e9ee7346f61c2d0c9ee87206
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-07-05 07:59:15 +0300

    create API urls paths for charity and charity list

--- adp/urls.py
+++ adp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('api/charities/', views.CharityList.as_view()),
+    path('api/charities/<int:pk>/', views.CharityDetails.as_view()),
 ]
--- adp/views.py
+++ adp/views.py
@@ -25,3 +25,36 @@ class CharityList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    #apply as a charity
+    def post(self, request, format=None):
+        serializer = CharitySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CharityDetails(APIView):
+    def get_object(self, pk):
+        try:
+            return Charity.objects.get(pk=pk)
+        except Charity.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        charity = self.get_object(pk)
+        serializer = CharitySerializer(charity)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        charity = self.get_object(pk)
+        serializer = CharitySerializer(charity, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        charity = self.get_object(pk)
+        charity.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
